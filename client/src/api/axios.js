00cfcf12310e53df932a2275fd1c525b461d2de1
giorgi tsartsidze <giotsaro@hotmail.com>
2025-06-04 14:00:50 +0400

    cross origin

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,6 +4,8 @@ const API = axios.create({
  // baseURL: "http://localhost:5000/api", // back-end URL
   baseURL: "https://nodereact-a43x.onrender.com/api", // back-end URL
   withCredentials: true, // cookie-ს გაგზავნა
+
+  
 });
 
 export default API;
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -39,12 +39,13 @@ export const login = async (req, res) => {
       { expiresIn: "1d" }
     );
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      sameSite: "strict",
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 24 * 60 * 60 * 1000,
-    });
+res.cookie("token", token, {
+    httpOnly: true,
+    sameSite: "None",  // ✅ Cross-Origin-ისთვის
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 24 * 60 * 60 * 1000,
+});
+
 
     res.status(200).json({
       message: "Login successful",
